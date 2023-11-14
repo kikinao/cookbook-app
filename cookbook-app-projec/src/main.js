637d24edf2717ac8d8d32ad06a9573cece581be2
xdy <xdy6965@163.com>
2023-11-14 09:38:47 +0800

    搭建了vuex

--- cookbook-app-projec/src/main.js
+++ cookbook-app-projec/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import vantConfig from './untils/vant-config';
@@ -11,5 +12,6 @@ Vue.use(vantConfig)
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
